7980e341439be0b05026040ba852e27948a63444
D M <darinm223@gmail.com>
2024-04-27 08:14:01 -0700

    InstructionSimplify folds constants and expands commutative binary operations

--- src/main/java/com/d_m/pass/InstructionSimplify.java
+++ src/main/java/com/d_m/pass/InstructionSimplify.java
@@ -1,10 +1,7 @@
 package com.d_m.pass;
 
 import com.d_m.code.Operator;
-import com.d_m.ssa.ConstantInt;
-import com.d_m.ssa.Constants;
-import com.d_m.ssa.Instruction;
-import com.d_m.ssa.Value;
+import com.d_m.ssa.*;
 import com.google.common.collect.Iterables;
 
 public class InstructionSimplify {
@@ -20,11 +17,19 @@ public class InstructionSimplify {
         return switch (operator) {
             case ADD -> simplifyAddInstruction(lhs, rhs, maxRecurse);
             case SUB -> simplifySubInstruction(lhs, rhs, maxRecurse);
+            case MUL -> simplifyMulInstruction(lhs, rhs, maxRecurse);
             default -> null;
         };
     }
 
     public static Value simplifyAddInstruction(Value operand1, Value operand2, int maxRecurse) {
+        FoldResult result = foldOrCommuteConstant(Operator.ADD, operand1, operand2);
+        operand1 = result.operand1;
+        operand2 = result.operand2;
+        if (result.constant != null) {
+            return result.constant;
+        }
+
         // X + 0 -> X
         if (operand2 instanceof ConstantInt constant && constant.getValue() == 0) {
             return operand1;
@@ -45,7 +50,41 @@ public class InstructionSimplify {
         return simplifyAssociativeBinOp(Operator.ADD, operand1, operand2, maxRecurse);
     }
 
+    public static Value simplifyMulInstruction(Value operand1, Value operand2, int maxRecurse) {
+        FoldResult result = foldOrCommuteConstant(Operator.MUL, operand1, operand2);
+        operand1 = result.operand1;
+        operand2 = result.operand2;
+        if (result.constant != null) {
+            return result.constant;
+        }
+
+        if (operand2 instanceof ConstantInt constant) {
+            // X * 0 -> 0
+            if (constant.getValue() == 0) {
+                return Constants.get(0);
+            }
+            // X * 1 -> X
+            if (constant.getValue() == 1) {
+                return operand1;
+            }
+        }
+        if (simplifyAssociativeBinOp(Operator.MUL, operand1, operand2, maxRecurse) instanceof Value v) {
+            return v;
+        }
+        if (simplifyCommutativeBinOp(Operator.MUL, operand1, operand2, Operator.ADD, maxRecurse) instance Value v){
+            return v;
+        }
+        return null;
+    }
+
     public static Value simplifySubInstruction(Value operand1, Value operand2, int maxRecurse) {
+        FoldResult result = foldOrCommuteConstant(Operator.SUB, operand1, operand2);
+        operand1 = result.operand1;
+        operand2 = result.operand2;
+        if (result.constant != null) {
+            return result.constant;
+        }
+
         // X - 0 -> X
         if (operand2 instanceof ConstantInt constant && constant.getValue() == 0) {
             return operand1;
@@ -156,5 +195,72 @@ public class InstructionSimplify {
             }
         }
         return null;
+    }
+
+    private static Value simplifyCommutativeBinOp(Operator operator, Value operand1, Value operand2, Operator operatorToExpand, int maxRecurse) {
+        if (maxRecurse-- <= 0) {
+            return null;
+        }
+        // (A opex B) op C
+        if (expandBinOpInstruction(operator, operand1, operand2, operatorToExpand, maxRecurse) instanceof Value v) {
+            return v;
+        }
+        // C op (A opex B)
+        if (expandBinOpInstruction(operator, operand2, operand1, operatorToExpand, maxRecurse) instanceof Value v) {
+            return v;
+        }
+        return null;
+    }
+
+    private static Value expandBinOpInstruction(Operator operator, Value operand1, Value operand2, Operator operatorToExpand, int maxRecurse) {
+        // (A opex B) op C -> (A op C) opex (B op C)
+        if (!(operand1 instanceof Instruction instruction) || instruction.getOperator() != operatorToExpand) {
+            return null;
+        }
+        Value a = instruction.getOperand(0).getValue();
+        Value b = instruction.getOperand(1).getValue();
+        if (!(simplifyBinOpInstruction(operator, a, operand2, maxRecurse) instanceof Value l)) {
+            return null;
+        }
+        if (!(simplifyBinOpInstruction(operator, b, operand2, maxRecurse) instanceof Value r)) {
+            return null;
+        }
+        // If A op C -> A and B op C -> B, then (A op C) opex (B op C) -> A opex B which is the first operand.
+        if ((l.equals(a) && r.equals(b)) || (operatorToExpand.isCommutative() && l.equals(b) && r.equals(a))) {
+            return operand1;
+        }
+        return simplifyBinOpInstruction(operatorToExpand, l, r, maxRecurse);
+    }
+
+    public record FoldResult(Constant constant, Value operand1, Value operand2) {
+    }
+
+    public static FoldResult foldOrCommuteConstant(Operator op, Value operand1, Value operand2) {
+        if (operand1 instanceof ConstantInt constant1 && operand2 instanceof ConstantInt constant2) {
+            return new FoldResult(constantFoldBinaryOp(op, constant1, constant2), operand1, operand2);
+        }
+        // Swap operands if operator is commutative.
+        if (op.isCommutative()) {
+            return new FoldResult(null, operand2, operand1);
+        }
+        return new FoldResult(null, operand1, operand2);
+    }
+
+    public static ConstantInt constantFoldBinaryOp(Operator op, ConstantInt constant1, ConstantInt constant2) {
+        return switch (op) {
+            case ADD -> Constants.get(constant1.getValue() + constant2.getValue());
+            case SUB -> Constants.get(constant1.getValue() - constant2.getValue());
+            case MUL -> Constants.get(constant1.getValue() * constant2.getValue());
+            case DIV -> Constants.get(constant1.getValue() / constant2.getValue());
+            case AND -> Constants.get(constant1.getValue() & constant2.getValue());
+            case OR -> Constants.get(constant1.getValue() | constant2.getValue());
+            case LT -> Constants.get(constant1.getValue() < constant2.getValue() ? 1 : 0);
+            case LE -> Constants.get(constant1.getValue() <= constant2.getValue() ? 1 : 0);
+            case GT -> Constants.get(constant1.getValue() > constant2.getValue() ? 1 : 0);
+            case GE -> Constants.get(constant1.getValue() >= constant2.getValue() ? 1 : 0);
+            case EQ -> Constants.get(constant1.getValue() == constant2.getValue() ? 1 : 0);
+            case NE -> Constants.get(constant1.getValue() != constant2.getValue() ? 1 : 0);
+            default -> null;
+        };
     }
 }
