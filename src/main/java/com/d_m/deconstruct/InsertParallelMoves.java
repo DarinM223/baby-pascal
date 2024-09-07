a189d24e12abf72d143e57382836419d4a41d83b
D M <darinm223@gmail.com>
2024-09-07 08:04:55 -0700

    Revert "Fix parallel move insertion to directly deconstruct SSA by not creating fresh operands"
    This reverts commit 3beda61c12201969c18879909bed046dc423d5a3.

--- src/main/java/com/d_m/deconstruct/InsertParallelMoves.java
+++ src/main/java/com/d_m/deconstruct/InsertParallelMoves.java
@@ -2,11 +2,12 @@ package com.d_m.deconstruct;
 
 import com.d_m.select.FunctionLoweringInfo;
 import com.d_m.select.instr.*;
+import com.d_m.select.reg.RegisterClass;
+import com.d_m.select.reg.RegisterConstraint;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 public class InsertParallelMoves {
     private final FunctionLoweringInfo info;
@@ -35,26 +36,23 @@ public class InsertParallelMoves {
         }
 
         for (MachineInstruction instruction : block.getInstructions()) {
-            Optional<MachineOperandPair> phiDefinitionPair = instruction
-                    .getOperands()
-                    .stream()
-                    .filter(pair -> pair.kind() == MachineOperandKind.DEF)
-                    .findFirst();
-            if (instruction.getInstruction().equals("phi") && phiDefinitionPair.isPresent()) {
-                MachineOperand phiDefinition = phiDefinitionPair.get().operand();
+            if (instruction.getInstruction().equals("phi")) {
                 int predecessorIndex = 0;
                 for (int i = 0; i < instruction.getOperands().size(); i++) {
                     MachineOperandPair pair = instruction.getOperands().get(i);
                     if (pair.kind() == MachineOperandKind.USE) {
+                        // TODO: use the type of the operand for the register class
+                        MachineOperand freshOperand = new MachineOperand.Register(info.createRegister(RegisterClass.INT, new RegisterConstraint.Any()));
                         MachineInstruction parallelMove = blockParallelMoveMap.get(block.getPredecessors().get(predecessorIndex));
                         parallelMove.getOperands().add(new MachineOperandPair(pair.operand(), MachineOperandKind.USE));
-                        parallelMove.getOperands().add(new MachineOperandPair(phiDefinition, MachineOperandKind.DEF));
+                        parallelMove.getOperands().add(new MachineOperandPair(freshOperand, MachineOperandKind.DEF));
+                        // Replace operand with freshOperand in the PHI node.
+                        instruction.getOperands().set(i, new MachineOperandPair(freshOperand, MachineOperandKind.USE));
+
                         predecessorIndex++;
                     }
                 }
             }
         }
-        // Clear all phi instructions.
-        block.getInstructions().removeIf(instruction -> instruction.getInstruction().equals("phi"));
     }
 }
